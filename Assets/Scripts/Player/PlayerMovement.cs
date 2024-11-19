3b16045dc32e441220ffb418995e4990058c88c1
Mitchell Opitz <mitchell@mitchellopitz.net>
2024-11-19 10:03:44 -0600

    feat - Player rotates towards mouse cursor

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,12 +16,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Movement
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+
+        // Calculate the desired new position
         Vector2 newPosition = rb.position + moveDirection * speed * Time.fixedDeltaTime;
 
         rb.MovePosition(newPosition);
+
+        // Facing the mouse
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDirection = mousePosition - rb.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+
+        // Rotate only on the Z axis
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
